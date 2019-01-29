b642cf5380996e9415145d99f7e19ce5e4006e9b
Clover <mar9b5@mail.missouri.edu>
2019-01-29 15:42:16 -0600

    added more to the portfolio page.

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ var artStatementButton = document.getElementById("artistStatementButton")
 var aboutPage = document.getElementById("aboutPage")
 var biography = document.getElementById("biography")
 var artistStatement = document.getElementById("artistStatement")
+var videosSection = document.getElementById("videosSection")
+var digitalArtSection = document.getElementById("digitalArtSection")
+var physicalArtSection = document.getElementById("physicalArtSection")
+
 
 var aboutPageIsShowing = false;
 var bioIsShowing = false;
 var artStatementIsShowing = false;
+var videosSectionIsShowing = false;
+var digitalIsShowing = false;
+var physicalIsShowing = false;
 
 
 function showBio() {
@@ -63,4 +70,52 @@ function showAbout() {
         artistStatement.style.display = "none"
     }
 
+}
+
+function showVideos() {
+    if (videosSectionIsShowing === false) {
+        videosSection.style.display = "inline-block"
+        videosSectionIsShowing = true;
+        digitalIsShowing = false;
+        digitalArtSection.style.display = "none"
+        physicalArtSection.style.display = "none"
+        physicalIsShowing = false;
+
+    }
+    else {
+        videosSection.style.display = "none"
+        videosSectionIsShowing = false;
+    }
+}
+
+function showDigital() {
+    if (digitalIsShowing === false) {
+        digitalArtSection.style.display = "inline-block"
+        digitalIsShowing = true;
+        videosSection.style.display = "none"
+        videosSectionIsShowing = false;
+        physicalArtSection.style.display = "none"
+        physicalIsShowing = false;
+
+    }
+    else {
+        digitalArtSection.style.display = "none"
+        digitalIsShowing = false;
+    }
+}
+
+function showPhysical() {
+    if (physicalIsShowing === false) {
+        physicalArtSection.style.display = "inline-block"
+        physicalIsShowing = true;
+        digitalIsShowing = false;
+        digitalArtSection.style.display = "none"
+        videosSection.style.display = "none"
+        videosSectionIsShowing = false;
+
+    }
+    else {
+        physicalIsShowing = false;
+        physicalArtSection.style.display = "none"
+    }
 }
