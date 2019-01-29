5093d9b6a18c5d144c445ac229c0ed49eadb2538
Clover <mar9b5@mail.missouri.edu>
2019-01-29 14:30:31 -0600

    made some edits. just look yourself.

--- index.js
+++ index.js
@@ -15,21 +15,10 @@ function showBio() {
     if (bioIsShowing === false) {
         biography.style.display = "inline-block"
         bioIsShowing = true;
-        artistStatement.style.display = "none"
-        aboutPage.style.display = "none"
-
-    }
-    else if (aboutPageIsShowing === true) {
-        biography.style.display = "inline-block"
-        aboutPage.style.display = "none"
-        aboutPageIsShowing = false;
-        bioIsShowing = true;
-    }
-    else if (artStatementIsShowing === true) {
-        biography.style.display = "inline-block"
-        artistStatement.style.display = "none"
         artStatementIsShowing = false;
-        bioIsShowing = true;
+        aboutPageIsShowing = false;
+        artistStatement.style.display = "none"
+        aboutPage.style.display = "none"
 
     }
     else {
@@ -45,20 +34,10 @@ function showArtStatement() {
     if (artStatementIsShowing === false) {
         artistStatement.style.display = "inline-block"
         artStatementIsShowing = true;
-        biography.style.display = "none"
-        aboutPage.style.display = "none"
-    }
-    else if (bioIsShowing === true) {
-        artistStatement.style.display = "inline-block"
-        biography.style.display = "none"
         bioIsShowing = false;
-        artStatementIsShowing = true;
-    }
-    else if (aboutPageIsShowing === true) {
-        artistStatement.style.display = "inline-block"
-        aboutPage.style.display = "none"
         aboutPageIsShowing = false;
-        artStatementIsShowing = true;
+        biography.style.display = "none"
+        aboutPage.style.display = "none"
     }
     else {
         artistStatement.style.display = "none"
@@ -72,21 +51,10 @@ function showAbout() {
     if (aboutPageIsShowing === false) {
         aboutPage.style.display = "inline-block";
         aboutPageIsShowing = true;
-        biography.style.display = "none"
-        artistStatement.style.display = "none"
-    }
-    else if (artStatementIsShowing === true) {
-        aboutPage.style.display = "inline-block"
-        artistStatement.style.display = "none"
-        artStatementIsShowing = false;
-        aboutPageIsShowing = true;
-    }
-    else if (bioIsShowing === true) {
-        aboutPage.style.display = "inline-block"
-        biography.style.display = "none"
         bioIsShowing = false;
-        aboutPageIsShowing = true;
-
+        artStatementIsShowing = false;
+        biography.style.display = "none"
+        artistStatement.style.display = "none"
     }
     else {
         aboutPage.style.display = "none"
