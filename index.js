76b8f2fcff45c79711f9e834fdb397043a568cd5
Clover <mar9b5@mail.missouri.edu>
2019-01-28 20:38:27 -0600

    changed some new stuff, so that's cool i guess

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
 // this is a js file
+var aboutButton = document.getElementById("aboutButton")
+var bioButton = document.getElementById("bioButton")
+var artStatementButton = document.getElementById("artistStatementButton")
+var aboutPage = document.getElementById("aboutPage")
+var biography = document.getElementById("biography")
+var artistStatement = document.getElementById("artistStatement")
+
+var aboutPageIsShowing = false;
+var bioIsShowing = false;
+var artStatementIsShowing = false;
+
+
+function showBio() {
+    if (bioIsShowing === false) {
+        biography.style.display = "inline-block"
+        bioIsShowing = true;
+
+    }
+    else if (aboutPageIsShowing === true) {
+        biography.style.display = "inline-block"
+        aboutPage.style.display = "none"
+        aboutPageIsShowing = false;
+        bioIsShowing = true;
+    }
+    else if (artStatementIsShowing === true) {
+        biography.style.display = "inline-block"
+        artistStatement.style.display = "none"
+        artStatementIsShowing = false;
+        bioIsShowing = true;
+
+    }
+    else {
+        biography.style.display = "none"
+        bioIsShowing = false;
+    }
+
+}
+
+function showArtStatement() {
+    if (artStatementIsShowing === false) {
+        artistStatement.style.display = "inline-block"
+        artStatementIsShowing = true;
+    }
+    else if (bioIsShowing === true) {
+        artistStatement.style.display = "inline-block"
+        biography.style.display = "none"
+        bioIsShowing = false;
+        artStatementIsShowing = true;
+    }
+    else if (aboutPageIsShowing === true) {
+        artistStatement.style.display = "inline-block"
+        aboutPage.style.display = "none"
+        aboutPageIsShowing = false;
+        artStatementIsShowing = true;
+    }
+    else {
+        artistStatement.style.display = "none"
+        artStatementIsShowing = false;
+    }
+}
+
+function showAbout() {
+    if (aboutPageIsShowing === false) {
+        aboutPage.style.display = "inline-block";
+        aboutPageIsShowing = true;
+    }
+    else if (artStatementIsShowing === true) {
+        aboutPage.style.display = "inline-block"
+        artistStatement.style.display = "none"
+        artStatementIsShowing = false;
+        aboutPageIsShowing = true;
+    }
+    else if (bioIsShowing === true) {
+        aboutPage.style.display = "inline-block"
+        biography.style.display = "none"
+        bioIsShowing = false;
+        aboutPageIsShowing = true;
+
+    }
+    else {
+        aboutPage.style.display = "none"
+        aboutPageIsShowing = false;
+    }
+
+}
